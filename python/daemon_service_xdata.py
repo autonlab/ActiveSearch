f500ba266dc40eb6ce5b65913ab29b520115b06e
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2016-01-14 15:12:23 -0500

    integrating xdata stuff

--- python/daemon_service_xdata.py
+++ python/daemon_service_xdata.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python2.7
 from flask import Flask
 from flask import Response
+
 import activeSearchInterface as asI
+import gaussianRandomFeatures as GRF
+
 import argparse
 import sys
+import os, os.path as osp
+import numpy as np
+import pandas as pd
 from random import randint
 
 ##
@@ -14,10 +20,125 @@ from random import randint
 # ./daemon_service.py
 ##
 
+### Working with Seattle data currently.
+def load_blockgroups_data (field_info, city, dataframes_file, bg_file):
+        city_data = pd.read_hdf(dataframes_file, city)
+        blockgroups_data = pd.read_hdf(bg_file, 'blockgroups')
+
+        blockgroups = {}
+        field_features = {}
+
+        npermits = city_data.shape[0]
+
+        for ipermit in xrange(npermits):
+                permit = city_data.iloc[ipermit]
+                try:
+                        blkg = int(permit.blockgroup)
+                except Exception as e:
+                        print ("Warning: Found NaN blockgroup. Ignoring...")
+                        break
+                if blkg not in blockgroups:
+                        blockgroups[blkg] = []
+                blockgroups[blkg].append(permit)
+
+        for f in field_info:
+                field_features[f] = {'type':field_info[f]}
+                if field_info[f] == 'categorical':
+                        field_features[f]['mapping'] = {}
+                        for i,fnam in enumerate(np.unique(city_data[f].values)):
+                                field_features[f]['mapping'][fnam] = i
+                elif field_info[f] == 'numerical':
+                        field_features[f]['grf'] = GRF.GaussianRandomFeatures(dim=1, gammak=0.25, rn=50, sine=True)
+
+        return blockgroups, field_features
+
+
+def featurize_blockgroup (bg_data, field_features):
+        """
+        Featurize the permits based on the relevant fields.
+        Some fields will just end up as binary bit arrays (categorical fields).
+        Some others will appear as gaussian random features.
+
+        Then averages over all these for the blockgroup.
+        """
+        X = []
+        field_data = {f:[] for f in field_features}
+        for permit in bg_data:
+                x = []
+                for f in field_features:
+                        if field_features[f]['type'] == 'categorical':
+                                z = [0]*len(field_features[f]['mapping'])
+                                z[field_features[f]['mapping'][permit[f]]] = 1
+                                x.extend(z)
+                                field_data[f].append(z)
+
+                        elif field_features[f]['type'] == 'numerical':
+                                # Assuming log vautes
+                                v = np.max([1.0,float(permit[f])]) # killing the zero values 
+                                z = field_features[f]['grf'].computeRandomFeatures([np.log(v)])
+                                x.extend(z)
+                                field_data[f].append(v)
+                X.append(x)
+
+        X = np.array(X).mean(axis=0)
+        field_avg_data = {}
+        for f in field_data:
+                if field_features[f]['type'] == 'categorical':
+                        f_dict = {}
+                        Xf = np.array(field_data[f]).mean(axis=0).tolist()
+                        for c,idx in field_features[f]['mapping'].items():
+                                f_dict[c] = Xf[idx]
+                        field_avg_data[f] = f_dict
+                elif field_features[f]['type'] == 'numerical':
+                        f_dict = {}
+                        Xf = field_data[f]
+                        f_dict['mean'] = np.mean(Xf)
+                        f_dict['median'] = np.median(Xf)
+                        f_dict['std'] = np.std(Xf)
+                        field_avg_data[f] = f_dict
+
+        return X.tolist(), field_avg_data
+
+
+def aggregate_data_into_features (data, field_features, sparse=False):
+        """
+        Assuming that the data is of the format:
+        {id:[... permits ... ] for id in set_of_ids}
+        """
+        X = []
+        BGMap = {}
+        idx = 0
+        for bg in data:
+                Xf, fad = featurize_blockgroup(data[bg], field_features)
+                BGMap[idx] = {'id': bg, 'display_data':fad}
+                X.append(Xf)
+                idx += 1
+
+        X = np.array(X).T
+
+        return X, BGMap
+
+
+def format_dict(d, indent = 0):
+    res = ""
+    for key in d:
+        res += ("   " * indent) + str(key) + ":\n"
+        if not isinstance (d[key], dict):
+            res += ("   " * (indent + 1)) + str(d[key]) + "\n"
+        else:
+            indent += 1
+            res += format_dict(d[key], indent)
+            indent -= 1
+    return res+"\n"
+
+
+def display_blockgroup(bg_info):
+        print (format_dict(bg_info))
+
+
 app = Flask(__name__)
 
 parser = argparse.ArgumentParser()
-
 args = parser.parse_args()
 
 activeSearch = None
@@ -31,6 +152,17 @@ first_run = True
 # track the message ID that we're currently presenting the user for evaluation
 currentMessage = -1 
 
+data_dir = osp.join(os.getenv('HOME'), 'Research/Data/ActiveSearch/housing')
+bg_file = osp.join(data_dir, 'blockgroups.h5')
+dataframes_file = osp.join(data_dir, 'dataframes.h5')
+
+city = 'seattle'
+fields = ['category', 'permit_type', 'action_type', 'work_type', 'value']
+field_types = ['categorical','categorical','categorical','categorical','numerical']
+field_info = {f:ft for f,ft in zip(fields,field_types)}
+
+blockgroups, field_features = load_blockgroups_data (field_types, city, dataframes_file, bg_file)
+Xf, BGMap = aggregate_data_into_features (blockgroups, field_features)
 
 verbose = True
 sparse = False
@@ -38,6 +170,7 @@ pi = 0.5
 eta = 0.7
 prms = ASI.Parameters(pi=pi,sparse=sparse, verbose=True, eta=eta)
 activeSearch = asI.kernelAS(prms)
+activeSearch = activeSearch.initialize(Xf)
 
 #@app.route(...)
 #def login():
