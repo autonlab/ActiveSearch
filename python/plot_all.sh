d57ccc1b48d981c10c1ba4d23a34ae41a954c221
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2016-02-15 21:42:50 -0500

    uncommited code for kdd submission

---
+++ python/plot_all.sh
@@ -0,0 +1,8 @@
+python plot_kdd_expts.py --dset HIGGS --prev 5 --ptype lin --stdc 1 --save
+python plot_kdd_expts.py --dset covtype --prev 0.47 --ptype lin --stdc 1 --save --proj
+python plot_kdd_expts.py --dset covtype --prev 0.47 --ptype lin --stdc 1 --save
+python plot_kdd_expts.py --dset SUSY --prev 0.5 --ptype lin --stdc 0.25 --proj --save
+python plot_kdd_expts.py --dset SUSY --prev 0.5 --ptype lin --stdc 0.25 --save
+python plot_kdd_expts.py --dset HIGGS --prev 5 --ptype lin --stdc 1 --proj --save
+python plot_kdd_expts.py --dset HIGGS --prev 5 --ptype lin --stdc 1 --save
+
